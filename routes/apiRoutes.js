d13b53020eb1f6eb757126f9c2e62ad8e5e01e78
Alice Wu <xiw451@ucsd.edu>
2020-12-19 21:32:26 -0800

    updated apiRoutes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,38 +1,45 @@
 // LOAD DATA
 const path = require("path");
 const fs = require("fs");
-const uuid = require("uuid");
+const { v4: uuidv4 } = require('uuid');
 
-const noteFile = path.join(__dirname, "../db/db.json");
+const noteFile = ("../db/db.json");
 
-// ROUTING
+
 
 module.exports = function (app) {
   // get note data from db.json
   app.get("/api/notes", function (req, res) {
-    const noteData = JSON.parse(fs.readFileSync(noteFile, "utf-8"));
-    res.json(noteData);
+    res.json(noteFile);
   });
 
   // save note data to db.json
   app.post("/api/notes", function (req, res) {
-    const noteData = JSON.parse(fs.readFileSync(noteFile, "utf-8"));
-    let newNote = req.body;
+    newInput = {
+      id: uuidv4(),
+      title: req.body.title,
+      text: req.body.text
+    }
     // use uuid to generate a unique id
-    newNote.id = uuid.v4();
-    noteData.push(newNote);
+    console.log(uuidv4());
+    console.log(newInput);
+    noteFile.push(newInput);
     // update db.json with the added object
-    fs.writeFileSync(noteFile, JSON.stringify(noteData), "utf-8");
+    fs.writeFileSync(noteFile, JSON.stringify(JSON.parse(noteFile, null, 2)), "utf-8");
     res.json(true);
   });
 
   // delete an object from db.json by unique id
   app.delete("/api/notes/:id", function (req, res) {
-    const noteData = JSON.parse(fs.readFileSync(noteFile, "utf-8"));
-    // filter out the targeted object
-    const filteredNotes = noteData.filter((note) => note.id != req.params.id);
+    var drawdata = JSON.parse(data);
+     if(note.id != req.params.id) {
+       return true;
+     }
+     else {
+       return false;
+     }
     // update db.json with the updated array that excludes the deleted note
-    fs.writeFileSync(noteFile, JSON.stringify(filteredNotes), "utf-8");
+    fs.writeFileSync(noteFile, JSON.stringify(drawdata), "utf-8");
     res.json(true);
   });
 };
